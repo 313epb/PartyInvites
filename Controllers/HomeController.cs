9e2646dcba176006eb33f0abb49785d7bb5cad5d
Дерюгин Дмитрий Сергеевич <Dmitrii_Deriugin@epam.com>
2022-01-07 18:31:55 +0400

    [L1-P46] Добавлено представление MyView для HomeController
    Добавлен класс GuestResponse для фиксации ответа гостей.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,36 +2,35 @@
 using Microsoft.Extensions.Logging;
 using PartyInvites.Models;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace PartyInvites.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+		public IActionResult Index()
+		{
+			var hour = DateTime.Now.Hour;
+			ViewBag.Greeting = hour < 12 ? "GoodMorning" : "GoodAfternoon";
+			return View("MyView");
+		}
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+		public IActionResult Privacy()
+		{
+			return View();
+		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
 }
